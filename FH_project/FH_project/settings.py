af6773cccf65fcfde190a9b045c59c1655973081
vegaklin <ruslan.evn@mail.ru>
2024-05-23 01:13:51 +0300

    feat: fix

--- FH_project/FH_project/settings.py
+++ FH_project/FH_project/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'FH_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'user',
-        'USER': 'user',
-        'PASSWORD': '141414',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'user',
+#         'USER': 'user',
+#         'PASSWORD': '141414',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
