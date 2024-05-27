0844e86440dfe3e6d3675a9e2ad68f069a3920f8
Timur <tmrgmdv2004@gmail.com>
2024-05-27 14:51:19 +0300

    added requirements.txt

--- FH_project/FH_project/settings.py
+++ FH_project/FH_project/settings.py
@@ -200,6 +200,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'foodhelperproject@gmail.com'  
-
-
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
