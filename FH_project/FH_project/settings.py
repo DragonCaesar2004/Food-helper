8eb661765fd5948a6a2d0f106840074345726021
Timur <tmrgmdv2004@gmail.com>
2024-05-27 22:25:38 +0300

    moved on PostgreSQL

--- FH_project/FH_project/settings.py
+++ FH_project/FH_project/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'FH_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'user',
-#         'USER': 'user',
-#         'PASSWORD': os.getenv('PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fh_db',
+        'USER': 'user',
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
--- FH_project/users/models.py
+++ FH_project/users/models.py
@@ -11,3 +11,23 @@ class CustomUser(AbstractUser):
     allergies = models.TextField(blank=True)
     weight = models.FloatField()
     height = models.FloatField()
+
+
+from django.db import models
+
+class Meal(models.Model):
+    user_id = models.IntegerField()  # Идентификатор пользователя
+    type = models.CharField(max_length=100)  # Тип приема пищи (например, завтрак, обед, ужин)
+    time = models.TimeField()  # Время (часы:минуты)
+    rating = models.PositiveSmallIntegerField()  # Оценка от 0 до 10
+    name = models.CharField(max_length=200)  # Название 
+    quantity = models.IntegerField()  # Количество
+    quantity_type = models.CharField(max_length=100)  # Тип количества (например, граммы, миллилитры)
+    date_added = models.DateTimeField(auto_now_add=True)  # Дата добавления (время дата)
+
+    def __str__(self):
+        return f"{self.type} - {self.name}"
+
+# После добавления этой модели в models.py вашего приложения, выполните следующие команды:
+# python manage.py makemigrations
+# python manage.py migrate
