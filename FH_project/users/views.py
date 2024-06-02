666e9c4807d27489b7989db5a78147cf81ca6b86
Timur <tmrgmdv2004@gmail.com>
2024-06-03 00:51:44 +0300

    almost work docker

--- FH_project/users/views.py
+++ FH_project/users/views.py
@@ -121,28 +121,7 @@ class UpdateProfileView(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-
-
-
-
-
-
-
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
-# from .serializers import UserSerializer  # Создайте сериализатор для пользователя
-
-# class LoadMeals(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def get(self, request, *args, **kwargs):
-#         user = request.user
-#         serializer = MealSerializer(user)
-#         return Response(serializer.data)
-
-
-# views.py
+#########################################################
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
