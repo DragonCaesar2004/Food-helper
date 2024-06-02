b53705b27e846ea3061e29f40125b19183faaa15
Timur <tmrgmdv2004@gmail.com>
2024-06-02 03:56:18 +0300

    added async sending emails by RabbitMQ

--- FH_project/main/apps.py
+++ FH_project/main/apps.py
@@ -12,7 +12,21 @@ from threading import Thread
 class MainConfig(AppConfig):
     name = 'main'
 
+from threading import Lock
+
+class MainConfig(AppConfig):
+    name = 'main'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scheduler_started = False
+        self.scheduler_lock = Lock()
+
     def ready(self):
-        from reminder.reminder import start_scheduler
-        scheduler_thread = Thread(target=start_scheduler)
-        scheduler_thread.start()
+        with self.scheduler_lock:
+            if not self.scheduler_started:
+                self.scheduler_started = True
+                from reminder.reminder import start_scheduler
+                start_scheduler()
+                from reminder.reminder import get_elements_from_q
+                get_elements_from_q()
--- FH_project/reminder/reminder.py
+++ FH_project/reminder/reminder.py
@@ -3,42 +3,44 @@ import smtplib
 from email.message import EmailMessage
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-import logging
+#import logging
 import atexit
 from main.utils import get_user_emails
-import os
-from dotenv import load_dotenv
-load_dotenv()
-email_sender = "foodhelperproject@gmail.com"
-email_password = os.getenv('EMAIL_HOST_PASSWORD')
-email_subject = "Не забудьте правильно питаться!"
-email_text = 'http://127.0.0.1:8000/'
-context = ssl.create_default_context()
+from .sender import send_to_rabbitmq
 
-def reminder():
-    for email_receiver in get_user_emails():
-        em = EmailMessage()
-        em['From'] = email_sender
-        em['To'] = email_receiver
-        em['Subject'] = email_subject
-        em.set_content(email_text)
-        
-        with smtplib.SMTP_SSL('smtp.gmail.com', port=465, context=context) as smtp:
-            smtp.login(email_sender, email_password)
-            smtp.sendmail(email_sender, email_receiver, em.as_string())
-        print(f"Email sent successfully to {email_receiver}")
+
+
 
 def start_scheduler():
+    print('first')
+
     scheduler = BackgroundScheduler()
     scheduler.add_job(
-        reminder,
-        trigger=CronTrigger(hour=2, minute=36),  # Запуск каждый день в 01:30
+        send_to_rabbitmq,
+        trigger=CronTrigger(hour=3, minute=53),  # Запуск каждый день в 01:30
         id='daily_reminder',
         name='Daily Reminder Email',
         replace_existing=True,
     )
     scheduler.start()
-    logging.info("Scheduler started")
+    #logging.info("Scheduler started")context=context
+
+    # Обработка завершения работы для корректного завершения планировщика
+    atexit.register(lambda: scheduler.shutdown())
+
+from .receive import get_elems
+def get_elements_from_q():
+    print('second')
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        get_elems,
+        trigger=CronTrigger(hour=3, minute=54),  # Запуск каждый день в 01:30
+        id='daily_reminder',
+        name='Daily Reminder Email',
+        replace_existing=True,
+    )
+    scheduler.start()
+    #logging.info("Scheduler started")
 
     # Обработка завершения работы для корректного завершения планировщика
     atexit.register(lambda: scheduler.shutdown())
