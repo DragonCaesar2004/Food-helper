2891f0a94091bf65b961192c8a68538aaa2c7f70
vegaklin <ruslan.evn@mail.ru>
2024-06-03 10:47:45 +0300

    refactor: all views.py

--- FH_project/main/views.py
+++ FH_project/main/views.py
@@ -1,53 +1,36 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-# Create your views here.
-
-def index(request):
-    return render(request, 'index.html')
-
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 import g4f
 from g4f.client import Client
 from g4f.Provider import RetryProvider, Phind, FreeChatgpt, Liaobots
 
-
+def index(request):
+    return render(request, 'index.html')
 
 def profile(request):
     client = Client(
         provider=RetryProvider([Phind, FreeChatgpt, Liaobots], shuffle=False)
     )
     try:
-        # if the session does not have a messages key, create one
         if 'messages' not in request.session:
             request.session['messages'] = [
                 {"role": "assistant",
                  "content": "Привет! Я ИИ-помощник по питанию, чем я могу быть вам полезен?"},
             ]
         if request.method == 'POST':
-            # get the prompt from the form
             prompt = request.POST.get('prompt')
-            # get the additional text from the form
             additional_text = request.POST.get('additional_text', '')
-            # combine the prompt and the additional text
             full_prompt = f"{additional_text}\n{prompt}"
-            # get the temperature from the form
             temperature = float(request.POST.get('temperature', 0.1))
-            # append the prompt to the messages list
             request.session['messages'].append({"role": "user", "content": prompt})
-            # set the session as modified
             request.session.modified = True
-            # call the openai API
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[{"role": "user", "content": full_prompt}],
             )
-            # format the response
             formatted_response = response.choices[0].message.content
-            # append the response to the messages list
             request.session['messages'].append({"role": "assistant", "content": formatted_response})
             request.session.modified = True
-            # redirect to the home page
             context = {
                 'messages': request.session['messages'],
                 'prompt': '',
@@ -55,7 +38,6 @@ def profile(request):
             }
             return render(request, 'main.html', context)
         else:
-            # if the request is not a POST request, render the home page
             context = {
                 'messages': request.session['messages'],
                 'prompt': '',
@@ -64,16 +46,11 @@ def profile(request):
             return render(request, 'main.html', context)
     except Exception as e:
         print(e)
-        # if there is an error, redirect to the error handler
         return redirect('main:error_handler')
 
-
 def new_chat(request):
-    # clear the messages list
     request.session.pop('messages', None)
     return redirect('main:profile')
 
-
 def error_handler(request):
     return render(request, '404.html')
-
--- FH_project/users/views.py
+++ FH_project/users/views.py
@@ -1,18 +1,27 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from django.http import JsonResponse 
+from django.contrib.auth import get_user_model 
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth import get_user_model
-from .serializers import RegisterSerializer, UserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import CustomUser
+from .models import CustomUser, Meal, Food
+from .serializers import  RegisterSerializer, UserSerializer, PasswordResetSerializer, MealSerializer, FoodSerializer
+import g4f
+from g4f.client import Client
+from g4f.Provider import RetryProvider, Phind, FreeChatgpt, Liaobots
+import random
+import string  
+import json
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
-    
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -24,13 +33,11 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
-
     def get_object(self):
         return self.request.user
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairView.serializer_class
-
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         response.data['user'] = UserSerializer(request.user).data
@@ -38,7 +45,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
-
     def post(self, request):
         try:
             refresh_token = request.data["refresh"]
@@ -48,20 +54,6 @@ class LogoutView(APIView):
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.mail import send_mail
-from django.contrib.auth import get_user_model
-import random
-import string
-
-# Импорт вашего сериализатора
-from .serializers import PasswordResetSerializer
-
 def index(request):
     return render(request, 'index.html')
 
@@ -75,38 +67,29 @@ class PasswordResetAPIView(APIView):
         if serializer.is_valid():
             email = serializer.validated_data['mail_for_recovery']
             User = get_user_model()
-
             try:
                 user = User.objects.get(email=email)
-
-                # Генерация нового пароля
                 new_password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
                 user.set_password(new_password)
                 user.save()
                 print(email)
-                # Отправка письма с новым паролем
                 send_mail(
                     'Ваш новый пароль',
                     f'Вы сбросили пароль на сайте Food Helper,\n вот новый пароль: {new_password} \n \n С уважением, команда Food Helper',
-                    'foodhelperproject@gmail.com',  # Замените на реальный адрес отправителя
+                    'foodhelperproject@gmail.com',
                     [email],
                     fail_silently=False,
                 )
-
                 return Response({'message': 'A new password has been sent to your email.'}, status=status.HTTP_200_OK)
             except User.DoesNotExist:
                 return Response({'error': 'Email not registered in the system.'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
 class UpdateProfileView(APIView):
     permission_classes = [IsAuthenticated]
-
     def put(self, request):
         user = request.user
         data = request.data
-
         user.email = data.get('email', user.email)
         user.date_of_birth = data.get('date_of_birth', user.date_of_birth)
         user.goal = data.get('goal', user.goal)
@@ -115,22 +98,12 @@ class UpdateProfileView(APIView):
         user.allergies = data.get('allergies', user.allergies)
         user.weight = data.get('weight', user.weight)
         user.height = data.get('height', user.height)
-
         user.save()
-        
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-#########################################################
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Meal
-from .serializers import MealSerializer
-
 class UserMealsView(APIView):
     permission_classes = [IsAuthenticated]
-
     def get(self, request, *args, **kwargs):
         user = request.user
         print('hello')
@@ -138,52 +111,28 @@ class UserMealsView(APIView):
         serializer = MealSerializer(meals, many=True)
         return Response(serializer.data)
 
-
-
-    
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .models import Meal, Food
-from .serializers import MealSerializer, FoodSerializer
-
 class MealCreateView(generics.CreateAPIView):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
     permission_classes = [IsAuthenticated]
-
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
- 
 class FoodCreateView(generics.CreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticated]
     
-
-
-
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import g4f
-from g4f.client import Client
-from g4f.Provider import RetryProvider, Phind, FreeChatgpt, Liaobots
-import json
-
 @csrf_exempt
 def generate_description(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         food_name = data.get('food_name')
         quantity = data.get('quantity')
-
         mealType = data.get('mealType')
         food_type = data.get('food_type')
         prompt = data.get('prompt')
-
         client = Client(provider=RetryProvider([Phind, FreeChatgpt, Liaobots], shuffle=False))
-
         try:
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
@@ -195,16 +144,6 @@ def generate_description(request):
             return JsonResponse({'error': str(e)}, status=500)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=400)
- 
-
-
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
- 
-import g4f
-from g4f.client import Client
-from g4f.Provider import RetryProvider, Phind, FreeChatgpt, Liaobots
-import json
 
 @csrf_exempt
 def generate_description2(request):
@@ -212,13 +151,10 @@ def generate_description2(request):
         data = json.loads(request.body)
         food_name = data.get('food_name')
         quantity = data.get('quantity')
-
         mealType = data.get('mealType')
         food_type = data.get('food_type')
         prompt = data.get('prompt')
-
         client = Client(provider=RetryProvider([Phind, FreeChatgpt, Liaobots], shuffle=False))
-
         try:
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
@@ -231,34 +167,16 @@ def generate_description2(request):
             return JsonResponse({'error': str(e)}, status=500)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=400)
-    
-
-
-
-
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .models import Meal
-from .serializers import MealSerializer
 
 class UserMealsView(generics.ListAPIView):
     serializer_class = MealSerializer
     permission_classes = [IsAuthenticated]
-
     def get_queryset(self):
         return Meal.objects.filter(user=self.request.user)
-
-
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Meal
 
 class MealDeleteView(generics.DestroyAPIView):
     queryset = Meal.objects.all()
     permission_classes = [IsAuthenticated]
-
     def delete(self, request, *args, **kwargs):
         meal_id = kwargs.get('pk')
         meal = self.get_queryset().filter(id=meal_id, user=request.user).first()
@@ -267,49 +185,25 @@ class MealDeleteView(generics.DestroyAPIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .models import Food
-from .serializers import FoodSerializer
-
 class FoodListView(generics.ListCreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticated]
-
     def get_queryset(self):
         user = self.request.user
         return Food.objects.filter(meal__user=user)
-
     def perform_create(self, serializer):
         serializer.save()
- 
-
-
-
- # views.py
-
-# views.py
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Meal
-from .serializers import MealSerializer
 
 class MealUpdateView(generics.UpdateAPIView):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
     permission_classes = [IsAuthenticated]
-
     def patch(self, request, *args, **kwargs):
         meal_id = kwargs.get('pk')
         meal = self.get_queryset().filter(id=meal_id, user=request.user).first()
         if not meal:
             return Response({'error': 'Meal not found or not authorized'}, status=status.HTTP_404_NOT_FOUND)
-
         mark = request.data.get('mark')
         if mark:
             meal.mark = mark
